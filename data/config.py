f23d82ad5902085e5b92d663659a2940ce462bbe
Le Ba Cong <conglb@yahoo.com>
2020-12-16 14:01:57 +0700

    BB

--- data/config.py
+++ data/config.py
@@ -1,14 +1,8 @@
-"""
-Copyright (c) 2017 Max deGroot, Ellis Brown
-Released under the MIT license
-https://github.com/amdegroot/ssd.pytorch
-Updated by: Takuya Mouri
-"""
 # config.py
 import os.path
 
 # gets home dir cross platform
-HOME = os.path.expanduser("~")
+HOME = os.getcwd() #os.path.expanduser("~")
 
 # for making bounding boxes pretty
 COLORS = ((255, 0, 0, 128), (0, 255, 0, 128), (0, 0, 255, 128),
@@ -19,12 +13,8 @@ MEANS = (104, 117, 123)
 # SSD300 CONFIGS
 voc = {
     'num_classes': 21,
-    # handbook
-    #'lr_steps': (80000, 100000, 120000),
-    #'max_iter': 120000,
-    'lr_steps': (8000, 10000, 12000),
-    'max_iter': 12000,
-    # handbook
+    'lr_steps': (80000, 100000, 120000),
+    'max_iter': 120000,
     'feature_maps': [38, 19, 10, 5, 3, 1],
     'min_dim': 300,
     'steps': [8, 16, 32, 64, 100, 300],
@@ -49,4 +39,19 @@ coco = {
     'variance': [0.1, 0.2],
     'clip': True,
     'name': 'COCO',
+}
+
+voclite = {
+    'num_classes': 14,
+    'lr_steps': (80000, 100000, 120000),
+    'max_iter': 120000,
+    'feature_maps': [38, 19, 10, 5, 3, 1],
+    'min_dim': 300,
+    'steps': [8, 16, 32, 64, 100, 300],
+    'min_sizes': [30, 60, 111, 162, 213, 264],
+    'max_sizes': [60, 111, 162, 213, 264, 315],
+    'aspect_ratios': [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+    'variance': [0.1, 0.2],
+    'clip': True,
+    'name': 'VOClite',
 }
